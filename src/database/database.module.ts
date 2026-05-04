894a5d49bc467e100ce95b3fe00939808f4e7486
idgroup5858 <idgroup5858@gmail.com>
2026-05-04 15:17:01 +0500

    ev

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -9,7 +9,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
             host:"localhost",
             port:3306,
             username:"root",
-            password:"",
+            password:"root",
             database:"zamon",
             autoLoadEntities:true,
             synchronize:true
